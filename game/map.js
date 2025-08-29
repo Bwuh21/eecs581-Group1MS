3f55a5b9720542c97cf02f534421ce12da3df817
pashiav <pashiavang2022@gmail.com>
2025-08-29 16:02:45 -0500

    add colors to the grid nums

--- game/map.js
+++ game/map.js
@@ -1,241 +1,234 @@
 export class Map {
-	constructor (
-		width,
-		height,
-		game,
-	) {
-		this.w = width;
-		this.h = height;
-		this.game = game;
+  constructor(width, height, game) {
+    this.w = width;
+    this.h = height;
+    this.game = game;
 
-		// Create grid
-		this.grid = [];
-		for (let y = 0; y < this.h; y++) {
-			this.grid[y] = [];
-			for (let x = 0; x < this.w; x++) {
-				// [Visual Number (Surrounding bombs), Bomb, Flag?, Covered?]
-				this.grid[y][x] = [0, 0, 0, 1];
-			}
-		}
-	}
+    // Create grid
+    this.grid = [];
+    for (let y = 0; y < this.h; y++) {
+      this.grid[y] = [];
+      for (let x = 0; x < this.w; x++) {
+        // [Visual Number (Surrounding bombs), Bomb, Flag?, Covered?]
+        this.grid[y][x] = [0, 0, 0, 1];
+      }
+    }
+  }
 
-	generateBombs(
-		bombCount,
-		startX,
-		startY,
-	) {
-		// Generate random number of bombs
-		let count = 0;
+  generateBombs(bombCount, startX, startY) {
+    // Generate random number of bombs
+    let count = 0;
 
-		// Mark forbidden cells.
-		// Bombs shouldn't spawn near or adjacent to where the user clicked
-		const forbidden = new Set()
-        for (let dy = -1; dy <= 1; dy++) {
-            for (let dx = -1; dx <= 1; dx++) {
-				const x = startX + dx;
-				const y = startY + dy;
-                if (this.inMap(x, y)) forbidden.add(`${x},${y}`);
-            }
+    // Mark forbidden cells.
+    // Bombs shouldn't spawn near or adjacent to where the user clicked
+    const forbidden = new Set();
+    for (let dy = -1; dy <= 1; dy++) {
+      for (let dx = -1; dx <= 1; dx++) {
+        const x = startX + dx;
+        const y = startY + dy;
+        if (this.inMap(x, y)) forbidden.add(`${x},${y}`);
+      }
+    }
+
+    while (count < bombCount) {
+      const x = Math.floor(Math.random() * this.w);
+      const y = Math.floor(Math.random() * this.h);
+
+      // Don't spawn where user clicked
+      if (forbidden.has(`${x},${y}`)) continue;
+
+      // Generate new coordinate if bomb is already there
+      if (this.getCell(x, y, 1) === 1) continue;
+
+      // Place bomb
+      this.setCell(x, y, 1, 1);
+
+      // Update numbers of surrounding tiles
+      for (let dy = -1; dy <= 1; dy++) {
+        for (let dx = -1; dx <= 1; dx++) {
+          const cx = x + dx;
+          const cy = y + dy;
+          if (this.inMap(cx, cy)) {
+            this.setCell(cx, cy, 0, this.getCell(cx, cy, 0) + 1);
+          }
         }
+      }
 
-		while (count < bombCount) {
-			const x = Math.floor(Math.random() * this.w);
-			const y = Math.floor(Math.random() * this.h);
-			
-			// Don't spawn where user clicked
-            if (forbidden.has(`${x},${y}`)) continue;
+      count++;
+    }
+  }
 
-			// Generate new coordinate if bomb is already there
-			if (this.getCell(x, y, 1) === 1) continue;
+  setCell(x, y, i, v) {
+    if (!this.inMap(x, y)) return;
+    this.grid[y][x][i] = v;
 
-			// Place bomb
-			this.setCell(x, y, 1, 1);
+    this.updateCell(x, y);
+  }
 
-			// Update numbers of surrounding tiles
-			for (let dy = -1; dy <= 1; dy++) {
-				for (let dx = -1; dx <= 1; dx++) {
-					const cx = x + dx;
-					const cy = y + dy;
-					if (this.inMap(cx, cy)) {
-						this.setCell(cx, cy, 0, this.getCell(cx, cy, 0) + 1)
-					}
-				}
-			}
+  updateMap(x, y) {
+    for (let y = 0; y < this.h; y++) {
+      for (let x = 0; x < this.w; x++) {
+        this.updateCell(x, y);
+      }
+    }
+  }
 
-			count++;
-		}
-	}
-	
-	setCell (
-		x,
-		y,
-		i,
-		v,
-	) {
-		if (!this.inMap(x,y)) return;
-		this.grid[y][x][i] = v;
-		
-		this.updateCell(x, y);
-	}
+  updateCell(x, y) {
+    const btn = document.getElementById(`cell-${x}-${y}`);
+    if (!btn) {
+      console.warn(`No cell at (${x}, ${y})`);
+      return;
+    }
 
-	updateMap(
-		x,
-		y,
-	) {
-		for (let y = 0; y < this.h; y++) {
-			for (let x = 0; x < this.w; x++) {
-				this.updateCell(x, y);
-			}
-		}
-	}
+    // Flag
+    if (this.getCell(x, y, 2) === 1) {
+      btn.textContent = "🚩";
+      return;
+    }
 
-	updateCell(
-		x,
-		y,
-	) {
-		const btn = document.getElementById(`cell-${x}-${y}`);
-		if (!btn) {
-			console.warn(`No cell at (${x}, ${y})`)
-			return;
-		}
+    // Covered?
+    if (this.getCell(x, y, 3) === 1) {
+      btn.textContent = "⬛";
+      return;
+    }
 
-		// Flag
-		if (this.getCell(x, y, 2) === 1) {
-			btn.textContent = "🚩";
-			return;
-		}
+    // Bomb?
+    if (this.getCell(x, y, 1) === 1) {
+      btn.textContent = "💣";
+      return;
+    }
 
-		// Covered?
-		if (this.getCell(x, y, 3) === 1) {
-			btn.textContent = "⬛";
-			return;
-		}
+    const number = this.getCell(x, y, 0);
+    if (number > 0) {
+      btn.textContent = number;
+      // Assign colors by number
+      switch (number) {
+        case 1:
+          btn.style.color = "blue";
+          break;
+        case 2:
+          btn.style.color = "green";
+          break;
+        case 3:
+          btn.style.color = "red";
+          break;
+        case 4:
+          btn.style.color = "darkblue";
+          break;
+        case 5:
+          btn.style.color = "brown";
+          break;
+        case 6:
+          btn.style.color = "turquoise";
+          break;
+        case 7:
+          btn.style.color = "black";
+          break;
+        case 8:
+          btn.style.color = "gray";
+          break;
+      }
+    } else {
+      btn.textContent = "";
+    }
 
-		// Bomb?
-		if (this.getCell(x, y, 1) === 1) {
-			btn.textContent = "💣";
-			return;
-		}
+    // Uncovered, disable.
+    btn.disabled = true;
+  }
 
-		const number = this.getCell(x, y, 0);
-		if (number > 0) {
-			btn.textContent = number;
-		} else {
-			btn.textContent = "";
-		}
+  getCell(x, y, i) {
+    if (!this.inMap(x, y)) return;
 
-		// Uncovered, disable.
-		btn.disabled = true;
-	}
+    const btn = document.getElementById(`cell-${x}-${y}`);
+    if (!btn) return;
 
-	getCell (
-		x,
-		y,
-		i,
-	) {
-		if (!this.inMap(x,y)) return;
+    return this.grid[y][x][i];
+  }
 
-		const btn = document.getElementById(`cell-${x}-${y}`);
-		if (!btn) return;
+  inMap(x, y) {
+    return x >= 0 && y >= 0 && x < this.w && y < this.h;
+  }
 
-		return this.grid[y][x][i];
-	}
+  cellClicked(x, y) {
+    if (!this.game.started) {
+      // Start Game
+      this.game.start(x, y);
+    }
 
-	inMap(x,y) {
-		return (x >= 0 && y >= 0 && x < this.w && y < this.h)
-	}
+    // Don't do anything if there is a flag
+    if (this.getCell(x, y, 2) === 1) {
+      return;
+    }
 
-	cellClicked (
-		x,
-		y,
-	) {
-		if (!this.game.started) {
-			// Start Game
-			this.game.start(x, y);
-		}
+    // Uncover tile
+    if (this.getCell(x, y, 3) === 1) {
+      // TODO: Flood algorithm to uncover neighboring blank cells.
+      this.floodFill(x, y);
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-		// Don't do anything if there is a flag
-		if (this.getCell(x, y, 2) === 1) {
-			return;
-		}
+  cellRightClicked(x, y) {
+    // Place flag on covered tiles
+    if (this.getCell(x, y, 3) === 1) {
+      if (this.getCell(x, y, 2) === 0) {
+        // Place
+        this.setCell(x, y, 2, 1);
+      } else {
+        // Remove
+        this.setCell(x, y, 2, 0);
+      }
+    }
+  }
 
-		// Uncover tile
-		if (this.getCell(x, y, 3) === 1) {
-			// TODO: Flood algorithm to uncover neighboring blank cells.
-			this.floodFill(x, y)
-			return true;
-		} else {
-			return false;
-		}
-	}
+  floodFill(startX, startY) {
+    const empty = this.getCell(startX, startY, 0) === 0;
+    // Clear first cell
+    this.setCell(startX, startY, 3, 0);
 
-	cellRightClicked (
-		x,
-		y,
-	) {
-		// Place flag on covered tiles
-		if (this.getCell(x, y, 3) === 1) {
-			if (this.getCell(x, y, 2) === 0) {
-				// Place
-				this.setCell(x, y, 2, 1);
-			} else {
-				// Remove
-				this.setCell(x, y, 2, 0);
-			}
-		}
-	}
+    // If empty, look for neighboring empty tiles.
+    if (empty) {
+      const visited = [];
+      for (let y = 0; y < this.h; y++) {
+        visited[y] = [];
+        for (let x = 0; x < this.w; x++) {
+          // [Visual Number (Surrounding bombs), Bomb, Flag?, Covered?]
+          visited[y][x] = false;
+        }
+      }
 
-	floodFill(
-		startX,
-		startY,
-	) {
-		const empty = this.getCell(startX, startY, 0) === 0;
-		// Clear first cell
-		this.setCell(startX, startY, 3, 0);
+      this._flood(startX, startY, visited);
+    }
+  }
 
-		// If empty, look for neighboring empty tiles.
-		if (empty) {
-			const visited = [];
-			for (let y = 0; y < this.h; y++) {
-				visited[y] = [];
-				for (let x = 0; x < this.w; x++) {
-					// [Visual Number (Surrounding bombs), Bomb, Flag?, Covered?]
-					visited[y][x] = false;
-				}
-			}
+  _flood(x, y, visited) {
+    // Clear cell and mark it as visited
+    this.setCell(x, y, 3, 0);
+    visited[y][x] = true;
 
-			this._flood(startX, startY, visited);
-		}
-	}
-	
-	_flood(
-		x, y, visited
-	) {
-		// Clear cell and mark it as visited
-		this.setCell(x, y, 3, 0);
-		visited[y][x] = true;
+    if (this.getCell(x, y, 0) !== 0) {
+      // Stop searching if not emptry
+      return;
+    }
 
-		if (this.getCell(x, y, 0) !== 0) {
-			// Stop searching if not emptry
-			return;
-		}
-
-		// Up
-		if (this.inMap(x, y-1) && !visited[y-1][x]) {
-			this._flood(x, y-1, visited);
-		}
-		// Left
-		if (this.inMap(x-1, y) && !visited[y][x-1]) {
-			this._flood(x-1, y, visited);
-		}
-		// Down
-		if (this.inMap(x, y+1) && !visited[y+1][x]) {
-			this._flood(x, y+1, visited);
-		}
-		// Right
-		if (this.inMap(x+1, y) && !visited[y][x+1]) {
-			this._flood(x+1, y, visited);
-		}
-		return;
-	}
+    // Up
+    if (this.inMap(x, y - 1) && !visited[y - 1][x]) {
+      this._flood(x, y - 1, visited);
+    }
+    // Left
+    if (this.inMap(x - 1, y) && !visited[y][x - 1]) {
+      this._flood(x - 1, y, visited);
+    }
+    // Down
+    if (this.inMap(x, y + 1) && !visited[y + 1][x]) {
+      this._flood(x, y + 1, visited);
+    }
+    // Right
+    if (this.inMap(x + 1, y) && !visited[y][x + 1]) {
+      this._flood(x + 1, y, visited);
+    }
+    return;
+  }
 }
