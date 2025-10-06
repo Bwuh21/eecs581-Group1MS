9abd0b082776b6ba4d7057a6ccb29942e271c35a
Bingus <164938543+Bingus669@users.noreply.github.com>
2025-10-05 20:10:27 -0500

    add comments draggable.js

--- game/draggable.js
+++ game/draggable.js
@@ -1,3 +1,15 @@
+/**
+ * Author: Reem Fatima, Pashia Vang, Alejandro Sandoval, Liam Aga, Jorge Trujillo, Aiden Barnard
+ * Authors Part 2: Maren Proplesch, Muhammad Ibrahim, Zach Corbin, Saurav Renju, Nick Grieco, Muhammad Abdulla. 
+ * Creation Date: 2025-10-01
+ * File: draggable.js
+ * Description: Handles draggable stickers on the main screen.
+ * Inputs/Outputs:
+ *   - None
+ * Responsibilities:
+ *   - Handles draggable stickers on the main screen
+ */
+
 document.querySelectorAll('.draggable').forEach(img => {
     let offsetX, offsetY, isDragging = false;
 
@@ -19,4 +31,5 @@ document.querySelectorAll('.draggable').forEach(img => {
         isDragging = false;
         img.style.cursor = 'grab';
     });
+
 });
